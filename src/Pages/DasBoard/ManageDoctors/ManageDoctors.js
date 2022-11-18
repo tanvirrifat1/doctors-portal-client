baaab684b15729ddc3444d3d88ea0244331de1bc
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2022-11-19 02:07:21 +0600

    added more

--- src/Pages/DasBoard/ManageDoctors/ManageDoctors.js
+++ src/Pages/DasBoard/ManageDoctors/ManageDoctors.js
@@ -1,9 +1,67 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import Loading from '../../Loading/Loading';
 
 const ManageDoctors = () => {
+    const { data: doctors, isLoading } = useQuery({
+        queryKey: ['doctors'],
+        queryFn: async () => {
+            try {
+                const res = await fetch('http://localhost:5000/doctors', {
+                    headers: {
+                        authorization: `bearer ${localStorage.getItem('accessToken')}`
+                    }
+                });
+                const data = await res.json();
+                return data;
+            }
+            catch (error) {
+
+            }
+        }
+    })
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
     return (
         <div>
-            <h2 className="text-3xl">Manage Doctors</h2>
+            <h2 className="text-3xl">Manage Doctors {doctors?.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr className='text-white'>
+                            <th></th>
+                            <th>Avatar</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Specialty</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            doctors?.map((doctor, i) =>
+                                <tr key={doctor._id}>
+                                    <th>{i + 1}</th>
+                                    <td><div className="avatar">
+                                        <div className="w-16 rounded-full">
+                                            <img src={doctor.image} alt="" />
+                                        </div>
+                                    </div></td>
+                                    <td>{doctor.name}</td>
+                                    <td>{doctor.email}</td>
+                                    <td>{doctor.specialty}</td>
+                                    <td><button className="btn btn-error btn-square">
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                                    </button></td>
+                                </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
